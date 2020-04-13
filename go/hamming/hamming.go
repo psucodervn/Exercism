af586aac91f4a0f3cb96e5b280e943010c880217
Hưng Lê <hungle.info@gmail.com>
2020-04-13 14:14:32 +0700

    Add go/hamming
    Signed-off-by: Hưng Lê <hungle.info@gmail.com>

---
+++ go/hamming/hamming.go
@@ -0,0 +1,16 @@
+package hamming
+
+import "errors"
+
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("invalid input")
+	}
+	diff := 0
+	for i := range a {
+		if a[i] != b[i] {
+			diff++
+		}
+	}
+	return diff, nil
+}
